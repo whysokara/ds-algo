f0118301f4f3cd2e0e3b4528312eca037c14fde4
Kara <kara_himanshu@yahoo.com>
2021-09-18 14:55:14 +0530

    Initial Commit

--- src/insertionSort.java
+++ src/insertionSort.java
@@ -10,7 +10,7 @@ public class insertionSort {
        for (int i = 0; i < arr.length - 1; i++) {
            for (int j = i+1; j > 0; j--) {
                if(arr[j] < arr[j-1]){
-                   swap(arr, j, j-1); 
+                   swap(arr, j, j-1);
                }
                else{
                    break;
