acb4194805f14db2e5b7e68a42f994e3cdc5008b
Kara <kara_himanshu@yahoo.com>
2021-10-05 00:35:38 +0530

    Fib updated

--- recursion/src/Fibo.java
+++ recursion/src/Fibo.java
@@ -1,11 +1,13 @@
 public class Fibo {
     public static void main(String[] args) {
-        System.out.println(Fib(4));
+        int ans = Fib(4);
+        System.out.println(ans);
     }
     static int Fib(int n){
         if(n < 2){
             return n;
         }
-        return Fib(n-1)+Fib(n-2);
+        return Fib(n-1) + Fib(n-2);
     }
 }
+
