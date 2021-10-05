d9ff2ab00b4979ffb1918cd00f594560400fbd41
Kara <kara_himanshu@yahoo.com>
2021-10-05 13:16:43 +0530

    Recursion Binary Search

---
+++ recursion/src/BinarySearch.java
@@ -0,0 +1,26 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 6, 8, 99};
+        int target = 3;
+        System.out.println(search(arr, target, 0, arr.length-1));
+
+
+    }
+
+    static int search(int[] arr, int target, int s, int e){
+        if(s > e){
+            return -1;
+        }
+
+        int m = s + (e - s) / 2;
+
+        if(arr[m] == target){
+            return m;
+        }
+
+        if(arr[m] < target){
+            return search(arr, target, m + 1, e);
+        }
+        return search(arr, target, s, m - 1);
+    }
+}
